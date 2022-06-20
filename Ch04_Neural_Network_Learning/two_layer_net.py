133c655dadae5ed71bfeb2c979cc0fa8e5d366f2
dayznight7 <dayznight7@gmail.com>
2022-06-20 18:43:34 +0900

    training NN using grad

--- Ch04_Neural_Network_Learning/two_layer_net.py
+++ Ch04_Neural_Network_Learning/two_layer_net.py
@@ -54,19 +54,19 @@ class TwoLayerNet:
         return grads
 
 
-net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
-print(net.params["W1"].shape)   # (784, 100)
-print(net.params["b1"].shape)   # (100,)
-print(net.params["W2"].shape)   # (100, 10)
-print(net.params["b2"].shape)   # (10,)
-
-x = np.random.rand(100, 784)
+# net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+# print(net.params["W1"].shape)   # (784, 100)
+# print(net.params["b1"].shape)   # (100,)
+# print(net.params["W2"].shape)   # (100, 10)
+# print(net.params["b2"].shape)   # (10,)
+#
+# x = np.random.rand(100, 784)
 # y = net.predict(x)
 # print(y)
-t = np.random.rand(100, 10)
-
-grads = net.numerical_gradient(x, t)
-print(grads["W1"].shape)
-print(grads["b1"].shape)
-print(grads["W2"].shape)
-print(grads["b2"].shape)
+# t = np.random.rand(100, 10)
+#
+# grads = net.numerical_gradient(x, t)
+# print(grads["W1"].shape)
+# print(grads["b1"].shape)
+# print(grads["W2"].shape)
+# print(grads["b2"].shape)
