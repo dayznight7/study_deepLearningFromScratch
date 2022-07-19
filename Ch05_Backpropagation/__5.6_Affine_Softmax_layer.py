d80fbca232e780ebc43a3cf152234f38ce081bf7
dayznight7 <dayznight7@gmail.com>
2022-07-19 19:35:41 +0900

    Affine, softmax layer

--- Ch05_Backpropagation/__5.6_Affine_Softmax_layer.py
+++ Ch05_Backpropagation/__5.6_Affine_Softmax_layer.py
@@ -11,7 +11,7 @@ print(dB)
 
 
 class Affine:
-    def __init__(self, w, b):
+    def __init__(self, W, b):
         self.W = W
         self.b = b
         self.x = None
@@ -23,4 +23,37 @@ class Affine:
         out = np.dot(x, self.W) + self.b
 
         return out
+
+
+# yk = exp(ak) / ∑(i=1 to n)(exp(ai))
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)  # 오버플로 대책
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
+def cross_entropy_error(y, t):
+    delta = 1e-7  # 0일때 -무한대가 되지 않기 위해 작은 값을 더함
+    return -np.sum(t * np.log(y + delta))
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
 
